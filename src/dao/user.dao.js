cf4458db7c37c7d02536819aa55e5948f4105257
kumuditha-udayanga <kumudithaudaya@gmail.com>
2025-05-01 23:22:45 +0530

    Create user dao

---
+++ src/dao/user.dao.js
@@ -0,0 +1,36 @@
+import db from "../config/db.config.js";
+import User from "../models/user.js";
+
+class UserDAO {
+  static createTable() {
+    const sql = `
+      CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+      )
+    `;
+    db.run(sql);
+  }
+
+  static create(user, callback) {
+    const sql = `INSERT INTO users (username, email, password) VALUES (?, ?, ?)`;
+    db.run(sql, [user.username, user.email, user.password], function(err) {
+      callback(err, this.lastID);
+    });
+  }
+
+  static findById(id, callback) {
+    const sql = `SELECT * FROM users WHERE id = ?`;
+    db.get(sql, [id], (err, row) => {
+      if (row) {
+        callback(err, new User(row.id, row.username, row.email, row.password));
+      } else {
+        callback(err, null);
+      }
+    });
+  }
+}
+
+module.exports = UserDAO;
