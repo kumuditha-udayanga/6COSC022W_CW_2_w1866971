d0fd74d88c2dc944379383c606b08e0695eb2bdf
kumuditha-udayanga <kumudithaudaya@gmail.com>
2025-05-10 23:57:30 +0530

    Implement create method

--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,4 @@
+import { userDao} from '../dao/user.dao.js';
 class User {
     constructor(id, username, email, password) {
       this.id = id;
@@ -5,6 +6,25 @@ class User {
       this.email = email;
       this.password = password;
     }
+
+    static async create({ email, password, username }) {
+      if (!email || !password || !username) {
+          throw new Error('Email, password, and username are required');
+      }
+      if (password.length < 6) {
+          throw new Error('Password must be at least 6 characters');
+      }
+      if (username.length < 3) {
+          throw new Error('Username must be at least 3 characters');
+      }
+      const existingUser = await userDao.findUserByEmail(email);
+      if (existingUser) {
+          throw new Error('Email already exists');
+      }
+      const userId = await userDao.createUser(email, password, username);
+      const userData = await userDao.findUserById(userId);
+      return new User(userData);
+  }
 }
   
 module.exports = User;  
