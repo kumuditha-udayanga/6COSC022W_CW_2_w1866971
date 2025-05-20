723ff71437589bb9b1a0e01b6dd9732af696f1f6
kumuditha-udayanga <kumudithaudaya@gmail.com>
2025-05-21 00:05:47 +0530

    Fix route not found issue

--- Backend/src/routes/blogRoutes.js
+++ Backend/src/routes/blogRoutes.js
@@ -5,10 +5,10 @@ import { requireAuth } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 router.post('/', requireAuth, createBlog);
+router.get('/search', searchBlogs);
+router.get('/recent', getRecentBlogs);
 router.get('/:id', getBlog);
 router.put('/:id', requireAuth, updateBlog);
 router.delete('/:id', requireAuth, deleteBlog);
-router.get('/search', searchBlogs);
-router.get('/recent', getRecentBlogs);
 
 export default router;
