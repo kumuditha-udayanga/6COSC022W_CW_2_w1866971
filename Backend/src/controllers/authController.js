294b35ced12582475d9baddd2e3d37b73d32ca83
kumuditha-udayanga <kumudithaudaya@gmail.com>
2025-05-21 16:14:29 +0530

    Fix logout function and link session dao

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -1,35 +1,40 @@
 import User from '../models/user.js';
-import { v4 as uuidv4 } from 'uuid';
+import SessionDao from "../dao/sessionDao.js";
+import {raw} from "express";
 
 export const sessions = {};
 
 export const register = async (req, res) => {
     try {
-        const { email, password, username } = req.body;
-        const user = await User.create({ email, password, username });
-        res.status(201).json({ message: 'User registered successfully', userId: user.id });
+        const {email, password, username} = req.body;
+        const user = await User.create({email, password, username});
+        res.status(201).json({message: 'User registered successfully', userId: user.id});
     } catch (error) {
-        res.status(400).json({ error: error.message });
+        res.status(400).json({error: error.message});
     }
 };
 
 export const login = async (req, res) => {
     try {
-        const { email, password } = req.body;
+        const {email, password} = req.body;
         const user = await User.findByEmail(email);
-        if (!user || !(await user.verifyPassword(password))) {
-            return res.status(401).json({ error: 'Invalid credentials' });
+        if (!user || !(await user.verifyPassword(password, user.password))) {
+            return res.status(401).json({error: 'Invalid credentials'});
         }
-        const apiKey = uuidv4();
+
+        const apiKey = await SessionDao.create(user);
         sessions[apiKey] = user.id;
-        res.cookie('apiKey', apiKey, { httpOnly: true });
-        res.json({ message: 'Login successful', userId: user.id });
+        res.cookie('apiKey', apiKey, {httpOnly: true});
+        res.json({message: 'Login successful', userId: user.id});
     } catch (error) {
-        res.status(500).json({ error: 'Login failed' });
+        res.status(500).json({error: 'Login failed'});
     }
 };
 
 export const logout = (req, res) => {
+    const apiKey = req.cookies.apiKey;
+    // console.log(apiKey)
+    SessionDao.delete(apiKey);
     res.clearCookie('apiKey');
-    res.json({ message: 'Logout successful' });
+    res.json({message: 'Logout successful'});
 };
